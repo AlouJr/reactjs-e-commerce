367ff408c3196c0db801d8d262d3ac0bca74c86b
Aloujr <alassanejuniorkebe@gmail.com>
2021-12-06 13:27:13 +0100

    React Ecommerce

---
+++ ecommerce/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './components/HomePage';
+import ProductList from './components/ProductList'
+
+function App() {
+  return (
+    <main>
+      <HomePage />
+      <ProductList />
+    </main>
+  );
+}
+
+export default App;
